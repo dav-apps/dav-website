0f9a1c0ddbe25f2387c928b9c106c0a6e032583f
David <dav2070@web.de>
2019-09-24 16:44:30 +0200

    Add acrylic background to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+	width: number = 500;
+	offsetTop: number = 0;
+
+	ngOnInit(){
+		this.setSize();
+		window.onscroll = () => this.offsetTop = window.scrollY;
+	}
+
+	@HostListener('window:resize')
+	onResize(){
+		this.setSize();
+	}
+
+	setSize(){
+		this.width = window.outerWidth;
+	}
+}
