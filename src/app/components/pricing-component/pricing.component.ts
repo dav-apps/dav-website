5ca486d2c89214b2ab4f81293790d66cabc55407
David <dav2070@web.de>
2019-10-05 01:04:32 +0200

    Show appropriate buttons depending on the current plan of the user

--- src/app/components/pricing-component/pricing.component.ts
+++ src/app/components/pricing-component/pricing.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data-service';
 
 @Component({
 	selector: 'dav-website-pricing',
 	templateUrl: './pricing.component.html'
 })
 export class PricingComponent{
-	
+	constructor(
+		public dataService: DataService
+	){}
 }
