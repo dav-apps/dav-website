6fb7c2416cbec5157a3c713a5e0b92d833200312
David <dav2070@web.de>
2019-10-05 18:56:52 +0200

    Add form for sending password reset email without functionality

--- src/app/pages/password-reset-page/password-reset-page.component.ts
+++ src/app/pages/password-reset-page/password-reset-page.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { SetTextFieldAutocomplete } from 'src/app/services/data-service';
 
 @Component({
 	selector: 'dav-website-password-reset-page',
 	templateUrl: './password-reset-page.component.html'
 })
 export class PasswordResetPageComponent{
-	
+	email: string = "";
+
+	ngAfterViewInit(){
+		// Set the autocomplete attribute of the input element
+		setTimeout(() => {
+			SetTextFieldAutocomplete('email-text-field', 'email', true);
+		}, 1);
+	}
+
+	SendPasswordResetEmail(){
+		console.log("Hello World")
+	}
 }
