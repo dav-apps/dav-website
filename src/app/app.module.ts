6eb680aef2c163e71c4ade99bedea404c869891f
David <dav2070@web.de>
2020-04-02 22:48:34 +0200

    Add empty ApiService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { ChartsModule } from 'ng2-charts';
 
 // Services
 import { DataService } from './services/data-service';
+import { ApiService } from './services/api-service';
 import { WebsocketService } from './services/websocket-service';
 
 // Components
@@ -116,6 +117,7 @@ import { PurchasePageComponent } from 'src/app/pages/purchase-page/purchase-page
   	],
   	providers: [
 		DataService,
+		ApiService,
 		WebsocketService
 	],
   	bootstrap: [AppComponent]
