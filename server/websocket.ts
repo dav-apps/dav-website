aa2b07b01986b2dfd7c585652496914630680e0e
David <dav2070@web.de>
2021-01-13 18:33:51 +0100

    Update the server websocket code to use dav-npm v2.0
    - Format the code
    - Use the new Dav class and the new Auth constructor

--- server/websocket.ts
+++ server/websocket.ts
@@ -1,28 +1,32 @@
-import { InitStatic, DavEnvironment, Auth } from 'dav-npm';
-import * as authorization from './websocket/auth';
-import * as session from './websocket/session';
-import * as user from './websocket/user';
-import * as purchase from './websocket/purchase';
-import * as app from './websocket/app';
-import * as stripe from './websocket/stripe';
+import { Dav, Auth, Environment } from 'dav-npm'
+import * as authorization from './websocket/auth'
+import * as session from './websocket/session'
+import * as user from './websocket/user'
+import * as purchase from './websocket/purchase'
+import * as app from './websocket/app'
+import * as stripe from './websocket/stripe'
 
-var socket = null;
-export var auth: Auth;
+var socket = null
+export var auth: Auth
 
-export function init(s: any){
-	socket = s;
-	for(let name in authorization.sockets) socket.on(name, authorization.sockets[name]);
-	for(let name in session.sockets) socket.on(name, session.sockets[name]);
-	for(let name in user.sockets) socket.on(name, user.sockets[name]);
-	for(let name in purchase.sockets) socket.on(name, purchase.sockets[name]);
-	for(let name in app.sockets) socket.on(name, app.sockets[name]);
-	for(let name in stripe.sockets) socket.on(name, stripe.sockets[name]);
+export function init(s: any) {
+	socket = s
+	for (let name in authorization.sockets) socket.on(name, authorization.sockets[name])
+	for (let name in session.sockets) socket.on(name, session.sockets[name])
+	for (let name in user.sockets) socket.on(name, user.sockets[name])
+	for (let name in purchase.sockets) socket.on(name, purchase.sockets[name])
+	for (let name in app.sockets) socket.on(name, app.sockets[name])
+	for (let name in stripe.sockets) socket.on(name, stripe.sockets[name])
 
-	InitStatic(process.env.ENV == "production" ? DavEnvironment.Production : DavEnvironment.Development);
-	auth = new Auth(process.env.DAV_API_KEY, process.env.DAV_SECRET_KEY, process.env.DAV_UUID);
+	new Dav({ environment: process.env.ENV == "production" ? Environment.Production : Environment.Development })
+	auth = new Auth({
+		apiKey: process.env.DAV_API_KEY,
+		secretKey: process.env.DAV_SECRET_KEY,
+		uuid: process.env.DAV_UUID
+	})
 }
 
-export function emit(key: string, message: any){
-	if(!socket) return;
-	socket.emit(key, message);
+export function emit(key: string, message: any) {
+	if (!socket) return
+	socket.emit(key, message)
 }
