3ab1ee9ee1641f2b0c4be71a2ccde64c6ba30fac
David <dav2070@web.de>
2021-01-13 18:52:50 +0100

    Rewrite the LoginPage to always create sessions, using the new backend
    - Simplify the login with only two modes: login for the website, which creates a session for the website, and login for an app, which creates a session for an app
    - Remove code for normal login and implicit login
    - Remove logging and use the syntax for dav-npm v2.0

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,46 +1,39 @@
-import { Component, HostListener } from '@angular/core';
-import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
-import { MessageBarType, IButtonStyles, SpinnerSize } from 'office-ui-fabric-react';
+import { Component, HostListener } from '@angular/core'
+import { Router, ActivatedRoute, NavigationExtras } from '@angular/router'
+import { MessageBarType, IButtonStyles, SpinnerSize } from 'office-ui-fabric-react'
 import {
+	Dav,
 	ApiResponse,
 	ApiErrorResponse,
-	LoginResponseData,
-	CreateSessionWithJwt,
-	CreateSessionResponseData,
-	Log
-} from 'dav-npm';
-import { DataService, SetTextFieldAutocomplete } from 'src/app/services/data-service';
-import { WebsocketService, WebsocketCallbackType } from 'src/app/services/websocket-service';
-import { environment } from 'src/environments/environment';
-import { enUS } from 'src/locales/locales';
-declare var deviceAPI: any;
+	CreateSessionResponseData
+} from 'dav-npm'
+import { DataService, SetTextFieldAutocomplete } from 'src/app/services/data-service'
+import { WebsocketService, WebsocketCallbackType } from 'src/app/services/websocket-service'
+import { environment } from 'src/environments/environment'
+import { enUS } from 'src/locales/locales'
+declare var deviceAPI: any
 
-const loginTypeImplicit = "implicit";
-const loginTypeSession = "session";
-const deviceInfoNotAvailable = "Not available";
-const loginEventName = "login";
-const loginImplicitEventName = "login_implicit";
-const loginSessionEventName = "login_session";
+const deviceInfoNotAvailable = "Not available"
 
 @Component({
 	selector: 'dav-website-login-page',
 	templateUrl: './login-page.component.html'
 })
-export class LoginPageComponent{
-	locale = enUS.loginPage;
-	loginType: LoginType = LoginType.Normal;
-	email: string = "";
-	password: string = "";
-	errorMessage: string = "";
-	appId: number = -1;
-	apiKey: string;
-	redirectUrl: string;
-	redirect: string;
-	loginLoading: boolean = false;
-	height: number = 400;
-	backButtonWidth: number = 40;
-	spinnerSize: SpinnerSize = SpinnerSize.small;
-	messageBarType: MessageBarType = MessageBarType.error;
+export class LoginPageComponent {
+	locale = enUS.loginPage
+	websiteLogin: boolean = true
+	email: string = ""
+	password: string = ""
+	errorMessage: string = ""
+	appId: number = -1
+	apiKey: string
+	redirectUrl: string
+	redirect: string
+	loginLoading: boolean = false
+	height: number = 400
+	backButtonWidth: number = 40
+	spinnerSize: SpinnerSize = SpinnerSize.small
+	messageBarType: MessageBarType = MessageBarType.error
 
 	loginButtonStyles: IButtonStyles = {
 		root: {
@@ -53,285 +46,198 @@ export class LoginPageComponent{
 		public websocketService: WebsocketService,
 		private router: Router,
 		private activatedRoute: ActivatedRoute
-	){
-		this.locale = this.dataService.GetLocale().loginPage;
-		let type = this.activatedRoute.snapshot.queryParamMap.get('type');
+	) {
+		this.locale = this.dataService.GetLocale().loginPage
 
-		if(type == loginTypeImplicit){
-			this.loginType = LoginType.Implicit;
-		}else if(type == loginTypeSession){
-			this.loginType = LoginType.Session;
-		}else{
-			this.loginType = LoginType.Normal;
-		}
+		this.appId = +this.activatedRoute.snapshot.queryParamMap.get("app_id")
+		this.apiKey = this.activatedRoute.snapshot.queryParamMap.get("api_key")
+		let redirectUrl = this.activatedRoute.snapshot.queryParamMap.get("redirect_url")
+		if (redirectUrl) this.redirectUrl = decodeURIComponent(redirectUrl).trim()
+		let redirect = this.activatedRoute.snapshot.queryParamMap.get("redirect")
+		if (redirect) this.redirect = decodeURIComponent(redirect).trim()
 
-		this.appId = +this.activatedRoute.snapshot.queryParamMap.get("app_id");
-		this.apiKey = this.activatedRoute.snapshot.queryParamMap.get("api_key");
-		let redirectUrl = this.activatedRoute.snapshot.queryParamMap.get("redirect_url");
-		if(redirectUrl) this.redirectUrl = decodeURIComponent(redirectUrl).trim();
-		let redirect = this.activatedRoute.snapshot.queryParamMap.get("redirect");
-		if(redirect) this.redirect = decodeURIComponent(redirect).trim();
+		// If none of the params are present, this is a normal login for the website
+		this.websiteLogin = this.appId == null && this.apiKey == null && this.redirectUrl == null
 
-		if(this.loginType == LoginType.Implicit){
-			// Check if apiKey and redirectUrl are present
-			if(!this.apiKey || this.apiKey.length < 2 || !this.redirectUrl || this.redirectUrl.length < 2) this.RedirectToStartPageWithError();
-			else this.dataService.hideNavbarAndFooter = true;
-		}else if(this.loginType == LoginType.Session){
-			// Check if appId, apiKey and redirectUrl are present
-			if(isNaN(this.appId) || this.appId <= 0 || !this.apiKey || this.apiKey.length < 2 || !this.redirectUrl || this.redirectUrl.length < 2) this.RedirectToStartPageWithError();
-			else this.dataService.hideNavbarAndFooter = true;
+		if (this.websiteLogin) {
+			// Set the appId, apiKey and redirectUrl
+			this.appId = environment.appId
+			this.apiKey = environment.apiKey
+		} else {
+			// Check if appId, apiKey and redirectUrl are present and valid
+			if (
+				isNaN(this.appId)
+				|| this.appId <= 0
+				|| this.apiKey == null
+				|| this.apiKey.length < 2
+				|| this.redirectUrl || this.redirectUrl.length < 2
+			) {
+				this.RedirectToStartPageWithError()
+				return
+			}
+
+			this.dataService.hideNavbarAndFooter = true
 		}
 	}
 
-	async ngOnInit(){
-		this.setSize();
-		await this.dataService.userPromise;
-		
-		if(this.dataService.user.IsLoggedIn && this.loginType == LoginType.Normal){
-			if(this.redirect){
+	async ngOnInit() {
+		this.setSize()
+		await this.dataService.userPromise
+
+		if (this.dataService.user != null && this.websiteLogin) {
+			if (this.redirect != null) {
 				// Redirect to the redirect url
-				this.router.navigateByUrl(this.redirect);
-			}else{
+				this.router.navigateByUrl(this.redirect)
+			} else {
 				// Redirect to the start page
-				this.router.navigate(['/']);
+				this.router.navigate(['/'])
 			}
 		}
 	}
 
-	ngAfterViewInit(){
+	ngAfterViewInit() {
 		// Set the autocomplete attribute of the input elements
 		setTimeout(() => {
-			SetTextFieldAutocomplete('email-text-field', 'email', true);
-			SetTextFieldAutocomplete('password-text-field', 'current-password');
-		}, 1);
+			SetTextFieldAutocomplete('email-text-field', 'email', true)
+			SetTextFieldAutocomplete('password-text-field', 'current-password')
+		}, 1)
 	}
 
 	@HostListener('window:resize')
-	onResize(){
-		this.setSize();
+	onResize() {
+		this.setSize()
 	}
 
-	setSize(){
-		this.height = window.innerHeight;
-		this.backButtonWidth = window.innerWidth < 576 ? 25 : 40;
+	setSize() {
+		this.height = window.innerHeight
+		this.backButtonWidth = window.innerWidth < 576 ? 25 : 40
 	}
 
-	async Login(){
-		this.errorMessage = "";
-		this.loginLoading = true;
+	async Login() {
+		this.errorMessage = ""
+		this.loginLoading = true
 
-		switch (this.loginType) {
-			case LoginType.Normal:
-				// Call login on the server
-				this.LoginResponse(
-					await this.websocketService.Emit(WebsocketCallbackType.Login, {
-						email: this.email,
-						password: this.password
-					})
-				)
-				break;
-			case LoginType.Implicit:
-				// Call loginImplicit on the server
-				this.LoginImplicitResponse(
-					await this.websocketService.Emit(WebsocketCallbackType.LoginImplicit, {
-						apiKey: this.apiKey,
-						email: this.email,
-						password: this.password
-					})
-				)
-				break;
-			case LoginType.Session:
-				// Get device info
-				let deviceName = this.locale.deviceInfoUnknown;
-				let deviceType = this.locale.deviceInfoUnknown;
-				let deviceOs = this.locale.deviceInfoUnknown;
-				
-				if(deviceAPI){
-					deviceName = deviceAPI.deviceName;
-					deviceType = this.Capitalize(deviceAPI.deviceType as string);
-					deviceOs = deviceAPI.osName;
-
-					if(deviceName == deviceInfoNotAvailable) deviceName = this.locale.deviceInfoUnknown;
-					if(deviceType == deviceInfoNotAvailable) deviceType = this.locale.deviceInfoUnknown;
-					if(deviceOs == deviceInfoNotAvailable) deviceOs = this.locale.deviceInfoUnknown;
-				}
-
-				// Create the session on the server
-				this.CreateSessionResponse(
-					await this.websocketService.Emit(WebsocketCallbackType.CreateSession, {
-						email: this.email,
-						password: this.password,
-						appId: this.appId,
-						apiKey: this.apiKey,
-						deviceName,
-						deviceType,
-						deviceOs
-					})
-				)
-				break;
-		}
-	}
-
-	async LoginAsLoggedInUser(){
 		// Get device info
-		let deviceName = this.locale.deviceInfoUnknown;
-		let deviceType = this.locale.deviceInfoUnknown;
-		let deviceOs = this.locale.deviceInfoUnknown;
+		let deviceName = this.locale.deviceInfoUnknown
+		let deviceType = this.locale.deviceInfoUnknown
+		let deviceOs = this.locale.deviceInfoUnknown
 
-		if(deviceAPI){
-			deviceName = deviceAPI.deviceName;
-			deviceType = this.Capitalize(deviceAPI.deviceType as string);
-			deviceOs = deviceAPI.osName;
+		if (deviceAPI) {
+			deviceName = deviceAPI.deviceName
+			deviceType = this.Capitalize(deviceAPI.deviceType as string)
+			deviceOs = deviceAPI.osName
 
-			if(deviceName == deviceInfoNotAvailable) deviceName = this.locale.deviceInfoUnknown;
-			if(deviceType == deviceInfoNotAvailable) deviceType = this.locale.deviceInfoUnknown;
-			if(deviceOs == deviceInfoNotAvailable) deviceOs = this.locale.deviceInfoUnknown;
+			if (deviceName == deviceInfoNotAvailable) deviceName = this.locale.deviceInfoUnknown
+			if (deviceType == deviceInfoNotAvailable) deviceType = this.locale.deviceInfoUnknown
+			if (deviceOs == deviceInfoNotAvailable) deviceOs = this.locale.deviceInfoUnknown
+		}
+
+		// Create the session on the server
+		this.CreateSessionResponse(
+			await this.websocketService.Emit(WebsocketCallbackType.CreateSession, {
+				email: this.email,
+				password: this.password,
+				appId: this.appId,
+				apiKey: this.apiKey,
+				deviceName,
+				deviceType,
+				deviceOs
+			})
+		)
+	}
+
+	async LoginAsLoggedInUser() {
+		// Get device info
+		let deviceName = this.locale.deviceInfoUnknown
+		let deviceType = this.locale.deviceInfoUnknown
+		let deviceOs = this.locale.deviceInfoUnknown
+
+		if (deviceAPI) {
+			deviceName = deviceAPI.deviceName
+			deviceType = this.Capitalize(deviceAPI.deviceType as string)
+			deviceOs = deviceAPI.osName
+
+			if (deviceName == deviceInfoNotAvailable) deviceName = this.locale.deviceInfoUnknown
+			if (deviceType == deviceInfoNotAvailable) deviceType = this.locale.deviceInfoUnknown
+			if (deviceOs == deviceInfoNotAvailable) deviceOs = this.locale.deviceInfoUnknown
 		}
 
 		// Create the session on the server
 		this.CreateSessionWithJwtResponse(
-			await CreateSessionWithJwt(
-				this.dataService.user.JWT,
-				this.appId,
-				this.apiKey,
+			await this.websocketService.Emit(WebsocketCallbackType.CreateSessionFromJwt, {
+				jwt: Dav.jwt,
+				appId: this.appId,
+				apiKey: this.apiKey,
 				deviceName,
 				deviceType,
 				deviceOs
-			)
+			})
 		)
 	}
 
-	GoBack(){
+	GoBack() {
 		// Redirect back to the app
-		window.location.href = this.redirectUrl;
+		window.location.href = this.redirectUrl
 	}
 
-	async LoginResponse(response: (ApiResponse<LoginResponseData> | ApiErrorResponse)){
-		if(response.status == 200){
-			// Save the jwt
-			await this.dataService.user.Login((response as ApiResponse<LoginResponseData>).data.jwt);
+	async CreateSessionResponse(response: (ApiResponse<CreateSessionResponseData> | ApiErrorResponse)) {
+		if (response.status == 201) {
+			// Redirect to the redirect url
+			window.location.href = `${this.redirectUrl}?jwt=${(response as ApiResponse<CreateSessionResponseData>).data.jwt}`
+		} else {
+			let errorCode = (response as ApiErrorResponse).errors[0].code
+			this.errorMessage = this.GetLoginErrorMessage(errorCode)
 
-			// Log the event
-			await this.LogEvent(loginEventName);
-
-			if(this.redirect){
-				// Redirect to the redirect url
-				this.router.navigateByUrl(this.redirect);
-			}else{
-				// Redirect to the start page
-				this.router.navigate(['/']);
-			}
-		}else{
-			let errorCode = (response as ApiErrorResponse).errors[0].code;
-			this.errorMessage = this.GetLoginErrorMessage(errorCode);
-
-			if(errorCode != 2106){
-				this.password = "";
+			if (errorCode != 2106) {
+				this.password = ""
 			}
 
 			// Hide the spinner
-			this.loginLoading = false;
+			this.loginLoading = false
 		}
 	}
 
-	async LoginImplicitResponse(response: (ApiResponse<LoginResponseData> | ApiErrorResponse)){
-		if(response.status == 200){
-			// Log the event
-			await this.LogEvent(loginImplicitEventName);
-
+	async CreateSessionWithJwtResponse(response: (ApiResponse<CreateSessionResponseData> | ApiErrorResponse)) {
+		if (response.status == 201) {
 			// Redirect to the redirect url
-			window.location.href = `${this.redirectUrl}?jwt=${(response as ApiResponse<LoginResponseData>).data.jwt}`;
-		}else{
-			let errorCode = (response as ApiErrorResponse).errors[0].code;
-			this.errorMessage = this.GetLoginErrorMessage(errorCode);
-
-			if(errorCode != 2106){
-				this.password = "";
-			}
-
-			// Hide the spinner
-			this.loginLoading = false;
+			window.location.href = `${this.redirectUrl}?jwt=${(response as ApiResponse<CreateSessionResponseData>).data.jwt}`
+		} else {
+			let errorCode = (response as ApiErrorResponse).errors[0].code
+			this.errorMessage = this.GetLoginErrorMessage(errorCode)
 		}
 	}
 
-	async CreateSessionResponse(response: (ApiResponse<CreateSessionResponseData> | ApiErrorResponse)){
-		if(response.status == 201){
-			// Log the event
-			await this.LogEvent(loginSessionEventName);
-
-			// Redirect to the redirect url
-			window.location.href = `${this.redirectUrl}?jwt=${(response as ApiResponse<CreateSessionResponseData>).data.jwt}`;
-		}else{
-			let errorCode = (response as ApiErrorResponse).errors[0].code;
-			this.errorMessage = this.GetLoginErrorMessage(errorCode);
-
-			if(errorCode != 2106){
-				this.password = "";
-			}
-
-			// Hide the spinner
-			this.loginLoading = false;
-		}
-	}
-
-	async CreateSessionWithJwtResponse(response: (ApiResponse<CreateSessionResponseData> | ApiErrorResponse)){
-		if(response.status == 201){
-			// Log the event
-			await this.LogEvent(loginSessionEventName);
-
-			// Redirect to the redirect url
-			window.location.href = `${this.redirectUrl}?jwt=${(response as ApiResponse<CreateSessionResponseData>).data.jwt}`;
-		}else{
-			let errorCode = (response as ApiErrorResponse).errors[0].code;
-			this.errorMessage = this.GetLoginErrorMessage(errorCode);
-		}
-	}
-
-	GetLoginErrorMessage(errorCode: number) : string{
+	GetLoginErrorMessage(errorCode: number): string {
 		switch (errorCode) {
 			case 1201:
-				return this.locale.errors.loginFailed;
+				return this.locale.errors.loginFailed
 			case 2106:
-				return this.locale.errors.emailMissing;
+				return this.locale.errors.emailMissing
 			case 2107:
-				return this.locale.errors.passwordMissing;
+				return this.locale.errors.passwordMissing
 			case 2801:
-				return this.locale.errors.loginFailed;
+				return this.locale.errors.loginFailed
 			default:
-				return this.locale.errors.unexpectedErrorShort.replace("{0}", errorCode.toString());
+				return this.locale.errors.unexpectedErrorShort.replace("{0}", errorCode.toString())
 		}
 	}
 
-	RedirectToStartPageWithError(){
-		this.dataService.startPageErrorMessage = this.locale.errors.unexpectedErrorLong;
-		this.router.navigate(['/']);
+	RedirectToStartPageWithError() {
+		this.dataService.startPageErrorMessage = this.locale.errors.unexpectedErrorLong
+		this.router.navigate(['/'])
 	}
 
-	NavigateToSignup(){
+	NavigateToSignup() {
 		let extras: NavigationExtras = {
 			state: {
 				redirectedFromLogin: true
 			}
 		}
-		
-		if(this.loginType == LoginType.Implicit){
-			this.router.navigateByUrl(`/signup?type=${loginTypeImplicit}&api_key=${this.apiKey}&redirect_url=${this.redirectUrl}`, extras);
-		}else if(this.loginType == LoginType.Session){
-			this.router.navigateByUrl(`/signup?type=${loginTypeSession}&api_key=${this.apiKey}&app_id=${this.appId}&redirect_url=${this.redirectUrl}`, extras);
-		}
+
+		this.router.navigateByUrl(`/signup?api_key=${this.apiKey}&app_id=${this.appId}&redirect_url=${this.redirectUrl}`, extras)
 	}
 
-	async LogEvent(name: string){
-		await Log(environment.apiKey, name);
+	Capitalize(s: string): string {
+		return s.charAt(0).toUpperCase() + s.slice(1)
 	}
-
-	Capitalize(s: string){
-		return s.charAt(0).toUpperCase() + s.slice(1);
-	}
-}
-
-enum LoginType{
-	Normal = 0,
-	Implicit = 1,
-	Session = 2
 }
