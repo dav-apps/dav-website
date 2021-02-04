979233981733e4eaf5a234df59cbbb1f58207ed8
David <dav2070@web.de>
2021-02-04 22:11:52 +0100

    Update AppCard to use updated attributes of App class
    - Format the code

--- src/app/components/app-card-component/app-card.component.ts
+++ src/app/components/app-card-component/app-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, Input } from '@angular/core';
-import { App } from 'dav-npm';
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
-import { faAndroid, faWindows } from '@fortawesome/free-brands-svg-icons';
+import { Component, Input } from '@angular/core'
+import { App } from 'dav-npm'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { faAndroid, faWindows } from '@fortawesome/free-brands-svg-icons'
 
 @Component({
 	selector: "dav-website-app-card",
 	templateUrl: './app-card.component.html'
 })
 export class AppCardComponent{
-	faGlobe = faGlobe;
-	faAndroid = faAndroid;
-	faWindows = faWindows;
-	@Input() app: App;
+	faGlobe = faGlobe
+	faAndroid = faAndroid
+	faWindows = faWindows
+	@Input() app: App
 }
