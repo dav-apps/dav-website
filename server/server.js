eba46a8f3177068b427ff4c8089757771be999c2
David <dav2070@web.de>
2021-10-15 17:55:50 +0200

    Update server.js to use ES6 module imports for express and socket io

--- server/server.js
+++ server/server.js
@@ -1,36 +1,39 @@
-var path = require('path');
-var express = require('express');
-require('dotenv').config();
+import * as path from 'path'
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import dotenv from 'dotenv'
+import * as websocket from './websocket.js'
 
-var websocket = require('./websocket');
+dotenv.config()
 
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const app = express()
+const http = createServer(app)
+const io = new Server(http)
 
 function getRoot(request, response) {
-	response.sendFile(path.resolve('./dav-website/index.html'));
+	response.sendFile(path.resolve('./dav-website/index.html'))
 }
 
 function getUndefined(request, response) {
-	response.sendFile(path.resolve('./dav-website/index.html'));
+	response.sendFile(path.resolve('./dav-website/index.html'))
 }
 
-app.use(express.static('./dav-website'));
+app.use(express.static('./dav-website'))
 
-app.get('/', getRoot);
-app.get('/*', getUndefined);
+app.get('/', getRoot)
+app.get('/*', getUndefined)
 
 io.on('connection', (socket) => {
 	// Check if the request has the referer header
-	let refererHeader = socket.handshake.headers.referer;
-	
-	if(!refererHeader || !refererHeader.startsWith(process.env.BASE_URL)){
-		// Close the connection
-		socket.disconnect();
-	}else{
-		websocket.init(socket);
-	}
-});
+	let refererHeader = socket.handshake.headers.referer
 
-http.listen(process.env.PORT || 3000);
+	if (!refererHeader || !refererHeader.startsWith(process.env.BASE_URL)) {
+		// Close the connection
+		socket.disconnect()
+	} else {
+		websocket.init(socket)
+	}
+})
+
+http.listen(process.env.PORT || 3000)
