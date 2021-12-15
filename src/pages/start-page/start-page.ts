feaee180e7722bb146c3d386ef0bfbbd46e6cfd6
David <dav2070@web.de>
2021-12-15 01:20:16 +0100

    Add support for avif and webp images on StartPage
    - Use picture element to serve images in avif and webp formats using parcel

--- src/pages/start-page/start-page.ts
+++ src/pages/start-page/start-page.ts
@@ -17,11 +17,17 @@ let largeHeaderUniversalsoundboard: HTMLHeadingElement
 let smallHeaderUniversalsoundboard: HTMLHeadingElement
 let largeHeaderCalendo: HTMLHeadingElement
 let smallHeaderCalendo: HTMLHeadingElement
+let screenshotPocketlibContainer: HTMLDivElement
 let screenshotPocketlib: HTMLImageElement
+let screenshotPocketlibMobileContainer: HTMLDivElement
 let screenshotPocketlibMobile: HTMLImageElement
+let screenshotUniversalsoundboardContainer: HTMLDivElement
 let screenshotUniversalsoundboard: HTMLImageElement
+let screenshotUniversalsoundboardMobileContainer: HTMLDivElement
 let screenshotUniversalsoundboardMobile: HTMLImageElement
+let screenshotCalendoContainer: HTMLDivElement
 let screenshotCalendo: HTMLImageElement
+let screenshotCalendoMobileContainer: HTMLDivElement
 let screenshotCalendoMobile: HTMLImageElement
 
 let loggedInHeader: HTMLHeadingElement
@@ -42,11 +48,17 @@ async function main() {
 	smallHeaderUniversalsoundboard = document.getElementById("small-header-universalsoundboard") as HTMLHeadingElement
 	largeHeaderCalendo = document.getElementById("large-header-calendo") as HTMLHeadingElement
 	smallHeaderCalendo = document.getElementById("small-header-calendo") as HTMLHeadingElement
+	screenshotPocketlibContainer = document.getElementById("screenshot-pocketlib-container") as HTMLDivElement
 	screenshotPocketlib = document.getElementById("screenshot-pocketlib") as HTMLImageElement
+	screenshotPocketlibMobileContainer = document.getElementById("screenshot-pocketlib-mobile-container") as HTMLDivElement
 	screenshotPocketlibMobile = document.getElementById("screenshot-pocketlib-mobile") as HTMLImageElement
+	screenshotUniversalsoundboardContainer = document.getElementById("screenshot-universalsoundboard-container") as HTMLDivElement
 	screenshotUniversalsoundboard = document.getElementById("screenshot-universalsoundboard") as HTMLImageElement
+	screenshotUniversalsoundboardMobileContainer = document.getElementById("screenshot-universalsoundboard-mobile-container") as HTMLDivElement
 	screenshotUniversalsoundboardMobile = document.getElementById("screenshot-universalsoundboard-mobile") as HTMLImageElement
+	screenshotCalendoContainer = document.getElementById("screenshot-calendo-container") as HTMLDivElement
 	screenshotCalendo = document.getElementById("screenshot-calendo") as HTMLImageElement
+	screenshotCalendoMobileContainer = document.getElementById("screenshot-calendo-mobile-container") as HTMLDivElement
 	screenshotCalendoMobile = document.getElementById("screenshot-calendo-mobile") as HTMLImageElement
 
 	loggedInHeader = document.getElementById("logged-in-header") as HTMLHeadingElement
@@ -153,9 +165,9 @@ function setSize() {
 			largeHeaderUniversalsoundboard,
 			largeHeaderCalendo,
 			// Show mobile screenshots
-			screenshotPocketlibMobile,
-			screenshotUniversalsoundboardMobile,
-			screenshotCalendoMobile
+			screenshotPocketlibMobileContainer,
+			screenshotUniversalsoundboardMobileContainer,
+			screenshotCalendoMobileContainer
 		)
 
 		hideElement(
@@ -164,9 +176,9 @@ function setSize() {
 			smallHeaderUniversalsoundboard,
 			smallHeaderCalendo,
 			// Hide desktop screenshots
-			screenshotPocketlib,
-			screenshotUniversalsoundboard,
-			screenshotCalendo
+			screenshotPocketlibContainer,
+			screenshotUniversalsoundboardContainer,
+			screenshotCalendoContainer
 		)
 	} else {
 		hideElement(
@@ -175,9 +187,9 @@ function setSize() {
 			largeHeaderUniversalsoundboard,
 			largeHeaderCalendo,
 			// Hide mobile screenshots
-			screenshotPocketlibMobile,
-			screenshotUniversalsoundboardMobile,
-			screenshotCalendoMobile
+			screenshotPocketlibMobileContainer,
+			screenshotUniversalsoundboardMobileContainer,
+			screenshotCalendoMobileContainer
 		)
 
 		showElement(
@@ -186,9 +198,9 @@ function setSize() {
 			smallHeaderUniversalsoundboard,
 			smallHeaderCalendo,
 			// Show desktop screenshots
-			screenshotPocketlib,
-			screenshotUniversalsoundboard,
-			screenshotCalendo
+			screenshotPocketlibContainer,
+			screenshotUniversalsoundboardContainer,
+			screenshotCalendoContainer
 		)
 	}
 }
