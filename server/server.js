84b46eae318212c42f2f9281aa6cd0d05ba357fb
David <dav2070@web.de>
2021-12-18 17:25:43 +0100

    Add post routes on server for API requests
    - Add functions for initializing dav-js and for checking for the Referer header
    - Add routes for createSession and createSessionFromAccessToken

--- server/server.js
+++ server/server.js
@@ -4,11 +4,20 @@ import { createServer } from 'http'
 import { Server } from 'socket.io'
 import dotenv from 'dotenv'
 dotenv.config()
+import {
+	Dav,
+	Auth,
+	Environment,
+	SessionsController,
+	UsersController
+} from 'dav-js'
 import * as websocket from './websocket.js'
 
 const app = express()
 const http = createServer(app)
 const io = new Server(http)
+var initialized = false
+var auth
 
 function getRoot(request, response) {
 	response.sendFile(path.resolve('./dav-website/index.html'))
@@ -19,9 +28,51 @@ function getUndefined(request, response) {
 }
 
 app.use(express.static('./dav-website'))
+app.use(express.json())
 
 app.get('/', getRoot)
 app.get('/*', getUndefined)
+
+app.post('/create_session', async (req, res) => {
+	if (!checkReferer(req, res)) return
+	init()
+
+	let response = await SessionsController.CreateSession({
+		auth,
+		email: req.body.email,
+		password: req.body.password,
+		appId: req.body.appId,
+		apiKey: req.body.apiKey,
+		deviceName: req.body.deviceName,
+		deviceOs: req.body.deviceOs
+	})
+
+	if (response.status == 201) {
+		res.status(response.status).send(response.data)
+	} else {
+		res.status(response.status).send(response.errors)
+	}
+})
+
+app.post('/create_session_from_access_token', async (req, res) => {
+	if (!checkReferer(req, res)) return
+	init()
+
+	let response = await SessionsController.CreateSessionFromAccessToken({
+		auth,
+		accessToken: req.body.accessToken,
+		appId: req.body.appId,
+		apiKey: req.body.apiKey,
+		deviceName: req.body.deviceName,
+		deviceOs: req.body.deviceOs
+	})
+
+	if (response.status == 201) {
+		res.status(response.status).send(response.data)
+	} else {
+		res.status(response.status).send(response.errors)
+	}
+})
 
 // Get the base urls from the environment variables
 const baseUrl = process.env.BASE_URL
@@ -43,5 +94,38 @@ io.on('connection', (socket) => {
 		websocket.init(socket)
 	}
 })
+
+function checkReferer(req, res) {
+	// Get the base urls from the environment variables
+	const baseUrl = process.env.BASE_URL
+	var baseUrls = []
+
+	if (baseUrl != null) {
+		baseUrls = baseUrl.split(',')
+	}
+
+	// Check if the request has the referer header
+	let refererHeader = req.headers.referer
+
+	// Check if the base urls contain the referer
+	if (!refererHeader || baseUrls.findIndex(baseUrl => refererHeader.startsWith(baseUrl)) == -1) {
+		res.sendStatus(403)
+		return false
+	}
+
+	return true
+}
+
+function init() {
+	if (initialized) return
+	initialized = true
+
+	new Dav({ environment: process.env.ENV == "production" ? Environment.Production : Environment.Development, server: true })
+	auth = new Auth({
+		apiKey: process.env.DAV_API_KEY,
+		secretKey: process.env.DAV_SECRET_KEY,
+		uuid: process.env.DAV_UUID
+	})
+}
 
 http.listen(process.env.PORT || 3000)
